3cd6b2539024498930c0999acafe683e79b3b070
mix <mhelabd@stanford.edu>
2019-07-13 10:28:36 -0700

    added team name

--- sample-bot.py
+++ sample-bot.py
@@ -13,7 +13,7 @@ import json
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # replace REPLACEME with your team name!
-team_name="REPLACEME"
+team_name="INDIANPYRAMIDS"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
 test_mode = True
@@ -43,6 +43,16 @@ def read_from_exchange(exchange):
 
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
+
+
+
+
+
+
+
+
+
+
 
 def main():
     exchange = connect()
